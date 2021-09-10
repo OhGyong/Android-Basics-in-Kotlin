3741b748ecf89687b0cd3b2bf7f40bd8d27e7116
OhGyong <oky7143@naver.com>
2021-09-11 03:11:13 +0900

    ViewModel 공부

--- Unit 3- Navigation/PATHWAY 3-Architecture components/android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ Unit 3- Navigation/PATHWAY 3-Architecture components/android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -21,6 +21,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.android.unscramble.R
 import com.example.android.unscramble.databinding.GameFragmentBinding
 
@@ -30,11 +31,14 @@ import com.example.android.unscramble.databinding.GameFragmentBinding
 class GameFragment : Fragment() {
 
     private var score = 0 // 점수
-    private var currentWordCount = 0  // 단어 수
+    private var currentWordCount = 0  // 플레이한 단어 개수
     private var currentScrambledWord = "test" // 글자가 뒤섞인 현재 단어
 
     // 바인딩 선언, onCreateView에서 binding을 사용하므로 lateinit 사용.
     private lateinit var binding: GameFragmentBinding
+
+    //GameViewModel 초기화
+    private val viewModel: GameViewModel by viewModels()
 
     /**
      * 바인딩을 사용하여 레이아웃 확장
@@ -49,49 +53,38 @@ class GameFragment : Fragment() {
 
 
     /**
-     * 요소들
+     * view들을 바인딩하여 리스너를 설정하고, ui를 업데이트
      */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Setup a click listener for the Submit and Skip buttons.
         binding.submit.setOnClickListener { onSubmitWord() }
         binding.skip.setOnClickListener { onSkipWord() }
-        // Update the UI
+
+        // UI 업데이트
         updateNextWordOnScreen()
         binding.score.text = getString(R.string.score, 0)
         binding.wordCount.text = getString(
                 R.string.word_count, 0, MAX_NO_OF_WORDS)
     }
 
-    /*
-    * Checks the user's word, and updates the score accordingly.
-    * Displays the next scrambled word.
-    */
+    /**
+     * submit 버튼 클릭 리스너 메서드
+     */
     private fun onSubmitWord() {
-        currentScrambledWord = getNextScrambledWord()
-        currentWordCount++
-        score += SCORE_INCREASE
-        binding.wordCount.text = getString(R.string.word_count, currentWordCount, MAX_NO_OF_WORDS)
-        binding.score.text = getString(R.string.score, score)
-        setErrorTextField(false)
-        updateNextWordOnScreen()
+
     }
 
-    /*
-     * Skips the current word without changing the score.
-     * Increases the word count.
+    /**
+     * skip 버튼의 클릭 리스너 메서드
      */
     private fun onSkipWord() {
-        currentScrambledWord = getNextScrambledWord()
-        currentWordCount++
-        binding.wordCount.text = getString(R.string.word_count, currentWordCount, MAX_NO_OF_WORDS)
-        setErrorTextField(false)
-        updateNextWordOnScreen()
+
     }
 
-    /*
-     * Gets a random word for the list of words and shuffles the letters in it.
+
+    /**
+     * 단어 목록에서 임의의 단어를 선택하여 섞는 메서드
      */
     private fun getNextScrambledWord(): String {
         val tempWord = allWordsList.random().toCharArray()
@@ -128,10 +121,11 @@ class GameFragment : Fragment() {
         }
     }
 
-    /*
-     * Displays the next scrambled word on screen.
+
+    /**
+     * 글자가 뒤섞인 새로운 단어를 표시하는 메서드
      */
     private fun updateNextWordOnScreen() {
-        binding.textViewUnscrambledWord.text = currentScrambledWord
+        binding.textViewUnscrambledWord.text = viewModel.currentScrambledWord
     }
 }
--- Unit 3- Navigation/PATHWAY 3-Architecture components/android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ Unit 3- Navigation/PATHWAY 3-Architecture components/android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,4 +1,18 @@
 package com.example.android.unscramble.ui.game
 
-class GameViewModel {
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() { // ViewModel은 추상 클래스
+
+    private var score = 0
+    private var currentWordCount = 0
+
+    /**
+     * private로 _currentScrambledWord의 이 클래스에서만 수정할 수 있도록 처리하고
+     * 외부 클래스에서 이 변수를 사용할 때 currentScrambledWord의 get() 메서드를 통해 처리한다.
+     */
+    private var _currentScrambledWord = "test"
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
+
 }
