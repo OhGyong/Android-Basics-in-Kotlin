a2786e2d3b839feabbeb9d4ead3ab755df40da0a
OhGyong <oky7143@naver.com>
2021-09-11 15:31:10 +0900

    View 모델 채우기

--- Unit 3- Navigation/PATHWAY 3-Architecture components/android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ Unit 3- Navigation/PATHWAY 3-Architecture components/android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -1,22 +1,7 @@
-/*
- * Copyright (C) 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and 
- * limitations under the License.
- */
-
 package com.example.android.unscramble.ui.game
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -48,6 +33,7 @@ class GameFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View {
         binding = GameFragmentBinding.inflate(inflater, container, false)
+        Log.d("GameFragment", "GameFragment created/re-created!")
         return binding.root
     }
 
@@ -127,5 +113,13 @@ class GameFragment : Fragment() {
      */
     private fun updateNextWordOnScreen() {
         binding.textViewUnscrambledWord.text = viewModel.currentScrambledWord
+    }
+
+    /**
+     * Activity와 Fragment가 소멸될 때 호출되는 메서드
+     */
+    override fun onDetach() {
+        super.onDetach()
+        Log.d("GameFragment", "GameFragment destroyed!")
     }
 }
--- Unit 3- Navigation/PATHWAY 3-Architecture components/android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ Unit 3- Navigation/PATHWAY 3-Architecture components/android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.android.unscramble.ui.game
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() { // ViewModel은 추상 클래스
@@ -7,12 +8,68 @@ class GameViewModel : ViewModel() { // ViewModel은 추상 클래스
     private var score = 0
     private var currentWordCount = 0
 
+    private var wordsList: MutableList<String> = mutableListOf() // 문제로 사용한 단어 리스트
+    private lateinit var currentWord: String // 현재 단어
+
     /**
      * private로 _currentScrambledWord의 이 클래스에서만 수정할 수 있도록 처리하고
      * 외부 클래스에서 이 변수를 사용할 때 currentScrambledWord의 get() 메서드를 통해 처리한다.
      */
-    private var _currentScrambledWord = "test"
+    private lateinit var _currentScrambledWord: String
     val currentScrambledWord: String
         get() = _currentScrambledWord
+
+    /**
+     * 전역 변수 선언 이후에 init을 실행해야 에러 안남
+     */
+    init {
+        Log.d("GameFragment", "GameViewModel created")
+        getNextWord()
+    }
+
+
+    /**
+     * 게임 중 같은 단어가 두 번 표시되지 않도록 설정
+     * 랜덤으로 섞인 단어가 원래의 단어랑 같을 경우 처리
+     */
+    private fun getNextWord() {
+        currentWord = allWordsList.random() // allWordList에서 랜덤으로 단어를 가져옴
+        val tempWord = currentWord.toCharArray() // 가져온 단어를 문자열 배열로 변환
+        tempWord.shuffle() // 문자열 배열로 변환한 것을 shuffle()로 글자 순서를 섞어준다.
+
+        // 섞인 단어가 원래으니 단어랑 일치하지 않도록 처리
+        while (tempWord.toString().equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+
+        // 게임 중 같은 단어가 두 번 표시되지 않도록 처리
+        if (wordsList.contains(currentWord)) {
+            getNextWord() // 포함되면 getNextWord()를 다시 호출하여 단어가 겹치지 않도록 처리.
+        } else {
+            _currentScrambledWord = String(tempWord) // 섞은 단어를 표시하도록 바꿔주고
+            ++currentWordCount // 단어 개수를 늘린다.
+            wordsList.add(currentWord) // 단어를 리스트에 추가한다.
+        }
+    }
+
+    /**
+     * ViewModel 내의 데이터를 처리하고 수정하는 도우미 메서드
+     */
+    fun nextWord(): Boolean {
+        return if (currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
+
+
+    /**
+     * ViewModel이 소멸되기 직전에 호출되는 메서드
+     */
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("GameFragment", "GameViewModel destroyed!")
+
+    }
 
 }
