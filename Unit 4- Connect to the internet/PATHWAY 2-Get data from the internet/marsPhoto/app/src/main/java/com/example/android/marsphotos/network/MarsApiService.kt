d4697d92ea30bee9a6c1c70b6eb7a70473c5afd8
OhGyong <oky7143@naver.com>
2021-10-15 20:00:30 +0900

    Retrofit 호출

---
+++ Unit 4- Connect to the internet/PATHWAY 2-Get data from the internet/marsPhoto/app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -0,0 +1,16 @@
+package com.example.android.marsphotos.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+
+private const val BASE_URL =
+    "https://android-kotlin-fun-mars-server.appspot.com"
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+
+class MarsApiService {
+
+
+}
