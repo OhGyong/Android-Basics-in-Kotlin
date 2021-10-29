e84254ee53786dde43d52e98ad32b8c5a9793bc5
OhGyong <oky7143@naver.com>
2021-10-29 18:15:09 +0900

    .

--- Unit 4- Connect to the internet/PATHWAY 2-Get data from the internet/marsPhoto/app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
+++ Unit 4- Connect to the internet/PATHWAY 2-Get data from the internet/marsPhoto/app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
@@ -13,13 +13,17 @@ import com.example.android.marsphotos.overview.PhotoGridAdapter
 /**
  * 앱 전반에 사용하는 Binding Adapter
  */
+
+
+/**
+ * MarsPhoto 객체 목록으로 PhotoGridAdapter를 초기화.
+ * BindingAdapter를 사용하여 RecyclerView 데이터를 설정하면 데이터 결합이 자동으로 MarsPhoto 객체 목록의 LiveData를 관찰하고
+ * MarsPhoto 목록이 변경되면 결합 어댑터가 자동으로 호출.
+ */
 @BindingAdapter("listData")
-fun bindRecyclerView(
-    recyclerView: RecyclerView,
-    data: List<MarsPhoto>?
-) {
+fun bindRecyclerView(recyclerView: RecyclerView, data: List<MarsPhoto>?) {
     val adapter = recyclerView.adapter as PhotoGridAdapter
-    adapter.submitList(data)
+    adapter.submitList(data) // 화성 사진 목록 데이터가 포함된 것을 호출
 }
 
 @BindingAdapter("imageUrl")
@@ -29,7 +33,7 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
         // URL 문자열을 Uri 객체로 변환하고 HTTPS 스키마 사용설정하면서 객체를 빌드.
         val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
 
-        // imgUri 객체에서 imgView로 이미지를 로드, Coil의 load(){}를 사용한 것.
+        // imgUri 객체에서 imgView로 이미지를 로드, Coil의 load(){}를 사용한 것. 후행람다 사용.
         imgView.load(imgUri) {
             placeholder(R.drawable.loading_animation)
             error(R.drawable.ic_broken_image)
--- Unit 4- Connect to the internet/PATHWAY 2-Get data from the internet/marsPhoto/app/src/main/java/com/example/android/marsphotos/overview/PhotoGridAdapter.kt
+++ Unit 4- Connect to the internet/PATHWAY 2-Get data from the internet/marsPhoto/app/src/main/java/com/example/android/marsphotos/overview/PhotoGridAdapter.kt
@@ -9,11 +9,14 @@ import com.example.android.marsphotos.databinding.GridViewItemBinding
 import com.example.android.marsphotos.network.MarsPhoto
 
 class PhotoGridAdapter :
-    ListAdapter<MarsPhoto, PhotoGridAdapter.MarsPhotosViewHolder>(DiffCallback) {
+    ListAdapter<MarsPhoto, PhotoGridAdapter.MarsPhotoViewHolder>(DiffCallback) {
 
-    class MarsPhotosViewHolder(
-        private var binding: GridViewItemBinding
-        ):RecyclerView.ViewHolder(binding.root) {
+    /**
+     * RecyclerView.ViewHolder를 확장하는 MarsPhotoViewHolder의 내부 클래스 정의를 추가.
+     * MarsPhoto를 레이아웃에 결합하기 위한 GridViewItemBinding 변수가 필요하기 때문에 이 변수를 MarsPhotoViewHolder에 전달.
+     * MarsPhotoViewHolder에서 MarsPhoto 객체를 인수로 사용하고 binding.proferty를 이 객체로 설정.
+     */
+    class MarsPhotoViewHolder(private var binding: GridViewItemBinding):RecyclerView.ViewHolder(binding.root) {
         fun bind(MarsPhoto: MarsPhoto) {
             binding.photo = MarsPhoto
             binding.executePendingBindings()
@@ -30,13 +33,18 @@ class PhotoGridAdapter :
         }
     }
 
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoGridAdapter.MarsPhotosViewHolder {
-        return MarsPhotosViewHolder(GridViewItemBinding.inflate(
+    /**
+     * GridViewItemBinding을 확장하고 상위 ViewGroup 컨텍스트의 LayoutInflater를 사용하여 생성된 새 MarsPhotoViewHolder를 반환
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MarsPhotoViewHolder {
+        return MarsPhotoViewHolder(GridViewItemBinding.inflate(
                 LayoutInflater.from(parent.context)))
     }
 
-    override fun onBindViewHolder(holder: PhotoGridAdapter.MarsPhotosViewHolder, position: Int) {
+    /**
+     * getItem()을 호출하여 현재 RecyclerView 위치와 연결된 MarsPhoto 객체를 가져온 다음 이 속성을 MarsPhotoViewHolder의 bind() 메서드에 전달
+     */
+    override fun onBindViewHolder(holder: MarsPhotoViewHolder, position: Int) {
         val marsPhoto = getItem(position)
         holder.bind(marsPhoto)
     }
